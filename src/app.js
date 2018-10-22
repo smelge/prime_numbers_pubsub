3888ce13badf19cfe90a38950c60622188c0a0bb
smelge <traildogfilms@gmail.com>
2018-10-22 19:46:24 +0100

    Collected input number

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-const InputText = require('./views/input_view.js')
+const InputText = require('./views/input_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript Loaded');
+  // console.log('JavaScript Loaded');
+  const inputText = new InputText();
+  inputText.bindEvents();
+
 });
--- src/views/input_view.js
+++ src/views/input_view.js
@@ -1,10 +1,17 @@
 const PubSub = require('../helpers/pubsub.js');
 
-const inputText = function(){
+const InputText = function(){
 
 };
 
-inputText.prototype.bindEvents = function(){
+InputText.prototype.bindEvents = function(){
   const form = document.querySelector('#prime-checker-form');
-  console.log('got form input');
+  // console.log('got form input');
+  form.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const inputtedText = event.target.number.value;
+    console.log('Input: ',inputtedText);
+  });
 };
+
+module.exports = InputText;
