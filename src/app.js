7d59bb9b0774c3a594505cfdc2c8eecbbceae72b
smelge <traildogfilms@gmail.com>
2018-10-22 19:37:29 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const InputText = require('./views/input_view.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+});
---
+++ src/views/input_view.js
@@ -0,0 +1,10 @@
+const PubSub = require('../helpers/pubsub.js');
+
+const inputText = function(){
+
+};
+
+inputText.prototype.bindEvents = function(){
+  const form = document.querySelector('#prime-checker-form');
+  console.log('got form input');
+};
